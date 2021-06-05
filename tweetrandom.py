fd0706069189e57b681713608709d94fb65eb4ab
Parker Higgins <parker@parkerhiggins.net>
2021-06-04 21:56:44 -0400

    got tweeting working

--- tweetrandom.py
+++ tweetrandom.py
@@ -1,18 +1,37 @@
 #!/usr/bin/env python3
 
-import csv, random, yaml, requests, os
+import csv
+import os
+
+import requests
+import yaml
+
 from io import BytesIO
 from twython import Twython
 from PIL import Image
 
+import maketweets
+
 fullpath = os.path.dirname(os.path.realpath(__file__))
+tweets_file = os.path.join(fullpath, 'shuffled_tweets.csv')
 
 with open(os.path.join(fullpath, "config.yaml")) as f:
     config = yaml.safe_load(f)
 
-with open(os.path.join(fullpath, "tweetswithimgs.csv")) as f:
-    tweetreader = csv.reader(f)
-    tweetlist = list(tweetreader)
+open(tweets_file, 'a').close()
+
+with open(tweets_file) as f:
+    reader = csv.DictReader(f)
+    tweets = list(reader)
+    if not tweets:
+        tweets = maketweets.main()
+
+tweet = tweets.pop(0)
+
+with open(tweets_file, "w") as f:
+    writer = csv.DictWriter(f, ['text', 'image_url'])
+    writer.writeheader()
+    writer.writerows(tweets)
 
 twitter_app_key = config['twitter_app_key']
 twitter_app_secret = config['twitter_app_secret']
@@ -21,27 +40,15 @@ twitter_oauth_token_secret = config['twitter_oauth_token_secret']
 
 twitter = Twython(twitter_app_key, twitter_app_secret, twitter_oauth_token, twitter_oauth_token_secret)
 
-atweet = random.choice(tweetlist)
+res = requests.get(tweet['image_url'])
 
-res = requests.get(atweet[1])
-photo = Image.open(BytesIO(res.content))
+img = Image.open(BytesIO(res.content))
+img_io = BytesIO()
+img.save(img_io, format='jpeg')
 
-wc_width, wc_height = photo.size
-long_edge = int(1.1 * max(wc_width,wc_height))
-
-paste_x = int(long_edge/2 - wc_width/2)
-paste_y = int(long_edge/2 - wc_height/2)
-
-bg = Image.new('RGB',(long_edge,long_edge),(255, 252, 233))
-bg.paste(photo,(paste_x, paste_y))
-
-image_io = BytesIO()
-
-bg.save(image_io,format='jpeg')
+img_io.seek(0)
 
 # Twitter upload, tweet
 
-image_io.seek(0)
-
-response = twitter.upload_media(media=image_io)
-twitter.update_status(status=atweet[0], media_ids=[response['media_id']])
+response = twitter.upload_media(media=img_io)
+twitter.update_status(status=tweet['text'], media_ids=[response['media_id']])
